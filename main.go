00563a3448d584180907c87b1e3a97a8dcad62f8
Fadhil Yaacob <mohamadfadhil.yaacob@onapp.com>
2020-09-25 12:20:50 +0800

    write extractToken function

--- main.go
+++ main.go
@@ -5,11 +5,10 @@ import (
 	"log"
 
 	"firebase.google.com/go"
-	// "firebase.google.com/go/auth"
 	"github.com/gin-gonic/gin"
 )
 
-func ValidateToken(c *gin.Context) {
+func validateToken(c *gin.Context) {
 
         ctx := context.Background()
         app, err := firebase.NewApp(ctx, nil)
@@ -22,25 +21,34 @@ func ValidateToken(c *gin.Context) {
 		log.Fatalf("error getting Auth client: %v\n", err)
 	}
 
-	idToken := c.Request.Header["Authorization"][0]
-	log.Printf("ID token: %v\n", idToken)
-
+        idToken := extractToken(c)
+        
 	token, err := client.VerifyIDToken(ctx, idToken)
 	if err != nil {
 		log.Fatalf("error verifying ID token: %v\n", err)
 	}
 
-	log.Printf("Verified ID token: %v\n", token)
-	c.JSON(200, gin.H{"token": idToken})
+        log.Printf("Verified ID token: %v\n", token)
+        
+	c.JSON(200, gin.H{
+          "X-Hasura-User-Id": token.uid,
+          "X-Hasura-Role": "user",
+        })
+}
+
+func extractToken(c *gin.Context) string {
+        authHeader := c.Request.Header["Authorization"][0]
+        log.Printf("ID token: %v\n", authHeader)
+        return authHeader
 }
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	r.GET("/healthz", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"OK",
 		})
 	})
-	r.GET("/auth", ValidateToken)
+	r.GET("/auth", validateToken)
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
